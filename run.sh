bbd95ad0ea105fb977dd9c97f10977206acbb49a
jieun <demonichje@gmail.com>
2018-09-07 14:00:39 +0900

    first commit

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source /cvmfs/cms.cern.ch/cmsset_default.sh 
+cd /cms/ldap_home/jieun/CMSSW_10_1_5/src 
+eval `scramv1 runtime -sh` 
+cd /cms/ldap_home/jieun/MuonSW
+
+root -l -b < x_file.C >& process.log  
