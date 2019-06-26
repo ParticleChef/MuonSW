007ce2733e758ea9c177f56d87ddd835946905fb
jieun <demonichje@gmail.com>
2019-06-27 07:12:44 +0900

    Signal window for official samples

--- MuonEff/RegionOfInterest.h
+++ MuonEff/RegionOfInterest.h
@@ -3,7 +3,26 @@
 
 double ROI_func(int region, double eget){
 	double p[5];
-
+/*****/
+	if(region == 1){
+		// fir for median
+		p[0] = -0.00206058;
+		p[1] = -0.336343;
+		p[2] = -0.300818;
+		p[3] = 0.299111;
+		p[4] = 0.836473;
+	}
+	if(region == 2){
+		// fir for median
+		p[0] = -0.000878987;
+		p[1] = -0.0909628;
+		p[2] = -0.800119;
+		p[3] = 0.170106;
+		p[4] = 2.40033;
+	}
+/*****/	
+//////////////// original //////////////////	
+/*****
 	if(region == 1){
 		// fir for median
 		p[0] = 0.000166057;
@@ -20,7 +39,8 @@ double ROI_func(int region, double eget){
 		p[3] = -0.097035;
 		p[4] = 3.13631;
 	}
-
+*****/
+/////////////////////////////////////////	
 
 	return p[0]*pow(eget,0) + p[1]*pow(eget,p[2])*exp(-pow(eget,p[3])+p[4]);
 }
